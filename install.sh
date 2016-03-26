df59408c76ab750aae993bf9773f7890938bdf9a
Christopher Bunn <b11.chris@gmail.com>
2016-03-26 00:28:41 -0700

    Removed double brackets in install.sh

--- install.sh
+++ install.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 platform='unknown'
 unamestr=`uname`
-if [[ "$unamestr" == 'Linux' ]]; then
+if [ "$unamestr" == 'Linux' ]; then
    platform='linux'
-elif [[ "$unamestr" == 'FreeBSD' ]]; then
+elif [ "$unamestr" == 'FreeBSD' ]; then
    platform='freebsd'
-elif [[ "$unamestr" == 'Darwin' ]]; then
+elif [ "$unamestr" == 'Darwin' ]; then
    platform='osx'
 fi
 
 # Clone source
-if [[ ! -d "dotfiles" ]]; then
+if [ ! -d "dotfiles" ]; then
     git clone https://github.com/mypetyak/dotfiles.git
 fi
 
 # ----- VIM -----
 mv ~/.vimrc ~/.vimrc_$(date -d "today" +"%Y%m%d%H%M")
 ln -s "$PWD"/dotfiles/.vimrc ~/.vimrc
-if [[ ! -d "`eval echo ~/.vim/bundle/Vundle.vim`" ]]; then
+if [ ! -d "`eval echo ~/.vim/bundle/Vundle.vim`" ]; then
     echo 
     git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
@@ -28,7 +28,7 @@ sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/i
 mv ~/.zshrc ~/.zshrc_$(date -d "today" +"%Y%m%d%H%M")
 ln -s "$PWD"/dotfiles/.zshrc ~/.zshrc
 mkdir -p ~/.oh-my-zsh/themes/
-if [[ ! -f "`eval echo ~/.oh-my-zsh/themes/bunn.zsh-theme`" ]]; then
+if [ ! -f "`eval echo ~/.oh-my-zsh/themes/bunn.zsh-theme`" ]; then
     ln -s "$PWD"/dotfiles/.oh-my-zsh/themes/bunn.zsh-theme ~/.oh-my-zsh/themes/bunn.zsh-theme || true
 fi
 
@@ -37,6 +37,6 @@ fi
 printf "[include]\n    path = %s/dotfiles/.gitconfig_include" "$PWD" >> ~/.gitconfig
 
 # ----- HOMEBREW -----
-if [[ $platform == 'osx' ]]; then
+if [ $platform == 'osx' ]; then
 	./brew.sh
 fi
